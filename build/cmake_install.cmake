fa7e38c63d6aaa57f45cff9ba310507fbfee2d3b
BogdanLibernoi <libernoi.bogdan@yandex.ru>
2024-03-21 22:07:57 +0300

    'Lesson2'

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/liber/OneDrive/Рабочий стол/OGL
+# Install script for directory: C:/Users/liber/OneDrive/Рабочий стол/OpenGL-Setup
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Ogl")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OpenGL-Setup")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,6 +32,16 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/liber/OneDrive/Рабочий стол/OpenGL-Setup/build/external/glfw/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/liber/OneDrive/Рабочий стол/OpenGL-Setup/build/external/glad/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/liber/OneDrive/Рабочий стол/OGL/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/liber/OneDrive/Рабочий стол/OpenGL-Setup/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,54 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(){
-    std::cout<< "Hello world!\n";
+
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+    {
+        std::cout<< "Glad load Error\n";
+        return -1;
+    }
+    std::cout<< "OpenGL Version " << GLVersion.major << "." << GLVersion.minor;
+
+    glClearColor(0, 1, 1, 1);
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
